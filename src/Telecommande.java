e5c4525430a26a5d3001a73eebebe29b35e7fe54
assalloca <hugo.assal1@etu.univ-lorraine.fr>
2023-04-07 17:04:25 +0200

    tele1

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -1,4 +1,34 @@
 public class Telecommande {
 
-    
+    private Lampe lampes;
+
+    public Telecommande(){
+
+
+    }
+
+    public void ajouterLampe(Lampe lampe){
+
+        throw new Error ("code non ecrit ");
+    }
+
+    public void activerLampe(int indiceLampe){
+
+        throw new Error ("code non ecrit ");
+    }
+
+    public void desactiverLampe(int indiceLampe){
+
+        throw new Error ("code non ecrit ");
+    }
+
+    public void activerTout(){
+
+        throw new Error ("code non ecrit ");
+    }
+
+    public String toString(){
+
+        throw new Error ("code non ecrit ");
+    }
 }
